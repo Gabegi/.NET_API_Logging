7046f5e9776e2eedaef6676182571797288af8ed
You have one new message <gabriel_pirastru@hotmail.fr>
2025-11-28 20:54:35 +0530

    order endpoint

---
+++ src/LoggingProduction/API/Endpoints/OrderEndpoints.cs
@@ -0,0 +1,30 @@
+using LoggingProduction.API.Handlers;
+
+namespace LoggingProduction.API.Endpoints;
+
+public static class OrderEndpoints
+{
+    public static void MapOrderEndpoints(this WebApplication app)
+    {
+        var handler = app.Services.GetRequiredService<OrderHandler>();
+
+        var group = app.MapGroup("/api/orders")
+            .WithTags("Orders");
+
+        group.MapGet("/", handler.GetAllOrders)
+            .WithName("GetAllOrders")
+            .WithOpenApi();
+
+        group.MapGet("/{id}", handler.GetOrderById)
+            .WithName("GetOrderById")
+            .WithOpenApi();
+
+        group.MapPost("/", handler.CreateOrder)
+            .WithName("CreateOrder")
+            .WithOpenApi();
+
+        group.MapGet("/search", handler.SearchOrders)
+            .WithName("SearchOrders")
+            .WithOpenApi();
+    }
+}
